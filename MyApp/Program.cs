8c5d5951af04aba03049d436e14a6196a6557f8d
Demis Bellot <demis.bellot@gmail.com>
2024-09-12 13:02:30 +0800

    Replace Background MQ with Background Jobs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MyApp.Areas.Identity.Pages.Account;
 using MyApp.ServiceInterface;
 
 var builder = WebApplication.CreateBuilder(args);
